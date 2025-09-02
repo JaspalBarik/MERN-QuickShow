78813c1496ddb4db843f98946a71925ad2969781
JASPAL BARIK <barikjaspal2001@gmail.com>
2025-09-02 18:39:53 +0530

    first commit

---
+++ client/src/main.jsx
@@ -0,0 +1,23 @@
+import './index.css';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
+import { ClerkProvider } from '@clerk/clerk-react'; // v5+ works with React 19
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key');
+}
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ClerkProvider>
+  </React.StrictMode>
+);
